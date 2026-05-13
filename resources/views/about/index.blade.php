dc299acb40ccd31b6af3c7e20248d3a29d5caf5b
Simon Tamene <128576645+ST-dev100@users.noreply.github.com>
2026-05-13 11:14:32 +0300

    first

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 class="hstyle">About Us</h1>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color: blue; font-size: 24px;">Welcome to Laravel!</h1>
+    </body>   
+</html>    
